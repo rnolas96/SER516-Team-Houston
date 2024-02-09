48d736736bdae21c5f2b090d2faab08b544c37bf
rnolas96 <rnolas96@asu.edu>
2024-02-08 21:56:06 -0700

    Lead Time UI Implementation

--- taigaProject/frontend/src/components/LeadTime.js
+++ taigaProject/frontend/src/components/LeadTime.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../App.css'
+import SidebarMenu from './SidebarMenu'
+import axios from 'axios'
+import BarChartMaker from './reusable_components/BarChartMaker';
 
 export default function LeadTime() {
+
+  const [averageCycleTimeLabels, setAverageCycleTimeLabels] = useState([]);
+  const [averageCycleTimeValues, setAverageCycleTimeValues] = useState([]);
+
+  useEffect (() => {
+
+    const labels = [];
+    const values = [];
+
+    const cycletimeUrl = "/api/cycletime";
+    // const data = axios.get(cycletimeUrl).then(res => {
+    //     res.data.map((key, value) => {
+    //       labels.push(key);
+    //       values.push(value);
+    //   })
+    // })
+  }, []);  
+
+  const data = {
+    labels: [ 'Sprint1', 'Sprint2', 'Sprint3', 'Sprint4', 'Sprint5'],
+    text: "Average lead time for each sprint",
+    datasets: [{
+      data: [15, 12, 21, 13, 13, 16]
+    }]
+  };
+
   return (
-    <div className='container'>This is the page for the Lead Time plotting.</div>
+    <div className='container-full'>
+      <SidebarMenu />
+      <div className='route-container'>        
+        <BarChartMaker props={data}/>
+      </div>
+    </div>
   )
 }
