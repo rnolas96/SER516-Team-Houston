138abc2ce122666c0381ac4236beb27f0538732b
avijay48 <avijay48@asu.edu.in>
2024-02-10 18:31:21 -0700

    removed unwanted line

--- taigaProject/backend/main.py
+++ taigaProject/backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from controller.userstory_controller import userstory_router
 from controller.task_controller import task_router
-from taigaApi.task.getTasks import get_closed_tasks_for_sprint
 
 app = FastAPI()
 app.include_router(userstory_router, prefix='/api/userstory')
