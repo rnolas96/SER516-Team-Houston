7e1e314668c52df1a00a1d6655e4af40437d17ca
avijay48 <avijay48@asu.edu.in>
2024-02-10 18:23:12 -0700

    added getcycletime endpoint and service and modified docker setuo

--- taigaProject/backend/main.py
+++ taigaProject/backend/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from controller.userstory_burndown_controller import router
-from controller.login_controller import router2
+from controller.userstory_controller import userstory_router
+from controller.task_controller import task_router
+from taigaApi.task.getTasks import get_closed_tasks_for_sprint
+
 app = FastAPI()
-app.include_router(router, prefix='/api')
-app.include_router(router2, prefix='/api')
-
-
-
-
+app.include_router(userstory_router, prefix='/api/userstory')
+app.include_router(task_router, prefix='/api/task')
