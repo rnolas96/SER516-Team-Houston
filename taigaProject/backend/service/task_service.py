b433eccd4880c537c65f5f9829fc3d7f917a4ec1
vshar117 <vshar117@asu.edu>
2024-02-11 14:22:36 -0700

    Added code for Sprintwise task

--- taigaProject/backend/service/task_service.py
+++ taigaProject/backend/service/task_service.py
@@ -1,8 +1,34 @@
 from itertools import groupby
-from taigaApi.task.getTasks import get_closed_tasks_for_sprint, get_closed_tasks
+from taigaApi.task.getTasks import get_closed_tasks_for_sprint, get_closed_tasks, get_all_tasks
 from taigaApi.task.getTaskHistory import get_task_history
 
-#function to calculate average cycle time for tasks which belong to a specific sprint
+
+def get_sprintwise_task_count(sprint_id, project_id, auth_token):
+    sprintwise_task_count = {}
+
+    tasks = get_all_tasks(project_id, auth_token)
+
+    for task in tasks:
+        sprint_id = task.get("milestone")
+
+        if sprint_id is not None:
+            sprint_task_count = sprintwise_task_count.get(sprint_id)
+            
+            if sprint_task_count is not None:
+                sprintwise_task_count[sprint_id] = sprint_task_count + 1
+            else:
+                sprintwise_task_count[sprint_id] = 1
+
+    for task in tasks:
+        sprint_id = task.get("milestone")
+
+        closed_task = get_closed_tasks_for_sprint(sprint_id)
+
+        sprintwise_task_count[sprint_id] = sprintwise_task_count[sprint_id] - len(closed_task)
+
+    return sprintwise_task_count
+
+#Function to calculate average cycle time for tasks which belong to a specific sprint
 def get_cycle_time_by_sprint_id(sprint_id, project_id, auth_token):
 
     closed_sprint_tasks = get_closed_tasks_for_sprint(sprint_id, project_id, auth_token)
@@ -11,20 +37,20 @@ def get_cycle_time_by_sprint_id(sprint_id, project_id, auth_token):
 
     return avg_cycle_time
 
-
-# Function to calculate and display  sprintwise average cycle time
+# Function to calculate and display sprintwise average cycle time
 def get_sprintwise_cycle_time(project_id, auth_token):
     tasks = get_closed_tasks(project_id, auth_token)
 
-    # sort the tasks milestone wise
+    # Sort the tasks milestone wise
     tasks.sort(key=lambda x: x["milestone_slug"])
     for key, group in groupby(tasks, key=lambda x: x["milestone_slug"]):
-        result={}
+        result ={ }
         cycle_time, closed_task = get_task_history(list(group), auth_token)
         avg_cycle_time = round((cycle_time / closed_task), 2)
 
         milestone_slug_split = str(key).split('-')
-        result["sprint"+milestone_slug_split[1]] = avg_cycle_time
+        result["sprint" + milestone_slug_split[1]] = avg_cycle_time
 
     return result
         
+
--- taigaProject/backend/taigaApi/task/getTasks.py
+++ taigaProject/backend/taigaApi/task/getTasks.py
@@ -61,13 +61,13 @@ def get_closed_tasks(project_id, auth_token):
     else:
         return None
     
-def get_closed_tasks_for_sprint(sprint_id,project_id,auth_token):
+def get_closed_tasks_for_sprint(sprint_id, project_id, auth_token):
 
-    #get tasks by calling get_tasks function
+    # Get tasks by calling get_tasks function
     tasks = get_tasks(project_id, auth_token)
-    if(tasks):
 
-        #filter tasks to include closed tasks for the given sprint
+    if tasks:
+        # Filter tasks to include closed tasks for the given sprint
         closed_tasks_list_for_sprint = [
             {
                 "id": task["id"],
@@ -78,7 +78,6 @@ def get_closed_tasks_for_sprint(sprint_id,project_id,auth_token):
                 "milestone_slug": task["milestone_slug"]
             }
             for task in tasks if task['milestone'] == sprint_id and task['is_closed']
-
         ]
 
         return closed_tasks_list_for_sprint
