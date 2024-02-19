a23d40f968ebd7271e31bc1af8f526ef4ad90b81
vshar117 <vshar117@asu.edu>
2024-02-18 19:15:30 -0700

    Replace sprint_id with sprint_name

--- taigaProject/backend/service/task_service.py
+++ taigaProject/backend/service/task_service.py
@@ -1,18 +1,21 @@
 from datetime import datetime, timedelta
 from itertools import groupby
-from taigaApi.task.getTasks import get_closed_tasks, get_all_tasks
+from taigaApi.task.getTasks import get_closed_tasks, get_milestone_name
 from taigaApi.task.getTaskHistory import get_lead_time
 
 def get_sprintwise_task_lead_time(project_id, auth_token):
     closed_tasks = get_closed_tasks(project_id, auth_token)
     closed_tasks_response = {}
 
+    milestones_name = get_milestone_name(project_id, auth_token)
+
     if closed_tasks is not None:
         for closed_task in closed_tasks:
             sprint_id = closed_task.get("milestone_id")
+            sprint_name = milestones_name[sprint_id]
 
-            if closed_tasks_response.get(sprint_id) is not None:
-                task_response = closed_tasks_response.get(sprint_id)
+            if closed_tasks_response.get(sprint_name) is not None:
+                task_response = closed_tasks_response.get(sprint_name)
                 task_response.append(
                     {
                         "task_id": closed_task.get("id"),
@@ -20,9 +23,9 @@ def get_sprintwise_task_lead_time(project_id, auth_token):
                         }
                 )
 
-                closed_tasks_response[sprint_id] = task_response 
+                closed_tasks_response[sprint_name] = task_response 
             else:
-                closed_tasks_response[sprint_id] = [{
+                closed_tasks_response[sprint_name] = [{
                     "task_id": closed_task.get("id"),
                      "lead_time": get_lead_time(closed_task, auth_token)
                 }]
