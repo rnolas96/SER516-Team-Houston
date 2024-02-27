baf5fdb073adb7ab7c67e24e47ae409c1a3289dc
avijay48 <avijay48@asu.edu.in>
2024-02-26 21:15:08 -0700

    added computaiton to calculate pb coupling and modified sb coupling response

--- taigaProject/backend/service/userstory_service.py
+++ taigaProject/backend/service/userstory_service.py
@@ -313,28 +313,33 @@ def partial_storypoint_burndown_for_sprint_process(sprint_id, auth_token):
 def get_sb_coupling(sprint_id, auth_token):
     result ={}
     userstory_ref_map={}
-
+    
+    nodes = []
+    edges = []
     user_stories = []
 
     try:
         custom_attribute_name = "DependsOn"
-        user_stories = get_userstories_by_sprint(sprint_id, auth_token)
-        if user_stories and len(user_stories)>0:
 
+        user_stories = get_userstories_by_sprint(sprint_id, auth_token)
+        custom_attribute_type_id = get_custom_attribute_type_id(user_stories[0]['project'], auth_token, custom_attribute_name)
+        
+        if user_stories and len(user_stories)>0:
             for userstory in user_stories:
                 userstory_ref_map[userstory['ref']] = userstory['id']
            
-           
             for userstory in user_stories:
+                user_story_obj={}
+                user_story_obj['id'] = userstory['id']
+                user_story_obj['label'] = userstory['ref']
+                user_story_obj['title'] = userstory['subject']
+                nodes.append(user_story_obj)
 
                 logging.info(f"userstory id :{userstory['id']}")         
                 logging.info(f"userstory subject :{userstory['subject']}")               
       
                 depends_on_values=[]
-                depends_on_list=[]
-
                 custom_attribute_data = get_custom_attribute_from_userstory(userstory['id'], auth_token)
-                custom_attribute_type_id = get_custom_attribute_type_id(userstory['project'], auth_token, custom_attribute_name)
 
                 logging.info(f"custom attribute data : {custom_attribute_data}")
                 logging.info(f"custom attribute type id : {custom_attribute_type_id}")
@@ -345,12 +350,13 @@ def get_sb_coupling(sprint_id, auth_token):
                     depends_on_values = re.findall(r"#(\d+)", depends_on_str)
 
                 for depends_on in depends_on_values:
-                    depends_on_list.append(userstory_ref_map[int(depends_on)])
-                    logging.info(f"depends on list", depends_on_list)
-
-                if len(depends_on_list) > 0:
-                    result[userstory['id']] =  depends_on_list
+                    edge = {}
+                    edge['from'] = userstory['id']
+                    edge['to'] = userstory_ref_map[int(depends_on)]
+                    edges.append(edge)
                 
+        result['nodes'] = nodes
+        result['edges'] = edges
         return result
 
 
@@ -365,19 +371,46 @@ def get_sb_coupling(sprint_id, auth_token):
 
 
 def get_pb_coupling(project_id, auth_token):
-
     result = {}
+    userstory_ref_map={}
+
     user_stories = []
-    pb_user_stories = []
+    nodes = []
+    edges = []
 
     try:
+        custom_attribute_name = "DependsOn"
         user_stories = get_user_story(project_id, auth_token)
 
-        for user_story in user_stories:
-            if(user_story['milestone']==None):
-                pb_user_stories.append(user_story)
+        if len(user_stories)>0:
+            custom_attribute_type_id = get_custom_attribute_type_id(user_stories[0]['project'], auth_token, custom_attribute_name)
 
-        return pb_user_stories
+            for user_story in user_stories:
+                userstory_ref_map[user_story['ref']] = user_story['id']
+
+            for user_story in user_stories:
+                if(user_story['milestone']==None):
+                    user_story_obj = {}
+                    user_story_obj['id'] = user_story['id']
+                    user_story_obj['label'] = user_story['ref']
+                    user_story_obj['title'] = user_story['subject']
+
+                    nodes.append(user_story_obj)
+                    custom_attribute_data = get_custom_attribute_from_userstory(user_story['id'], auth_token)
+                
+                    if(custom_attribute_type_id in custom_attribute_data):     
+                        depends_on_str = custom_attribute_data[custom_attribute_type_id]
+                        depends_on_values = re.findall(r"#(\d+)", depends_on_str)
+
+                        for depends_on in depends_on_values:
+                            edge = {}
+                            edge['from'] = user_story['id']
+                            edge['to'] = userstory_ref_map[int(depends_on)]
+                            edges.append(edge)
+
+        result['nodes'] = nodes
+        result['edges'] = edges                   
+        return result
 
     except UserStoryFetchingError as e:
         print(f"Error fetching UserStories: {e}")
