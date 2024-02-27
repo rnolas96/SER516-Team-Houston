4f4d2f7ebbdf82b255cd7ceae544f82b03ed244b
rnolas96 <rnolas96@asu.edu>
2024-02-27 09:00:19 -0700

    cost of delay business value and user story api complete

--- taigaProject/backend/service/task_service.py
+++ taigaProject/backend/service/task_service.py
@@ -3,6 +3,9 @@ from itertools import groupby
 from taigaApi.task.getTasks import TaskFetchingError, get_closed_tasks, get_milestone_name, get_tasks_by_milestone
 from taigaApi.task.getTaskHistory import get_cycle_time, get_lead_time
 from taigaApi.milestone.getMilestoneById import MilestoneFetchingError, get_milestone_by_id
+from taigaApi.issue.getIssuesByProjectId import get_issues_by_project_id
+from taigaApi.issue.getCustomAttributeValueByIssueId import get_custom_attribute_value_by_issue_id, get_custom_attribute_type_id_for_issue
+from taigaApi.userStory.getUserStory import get_userstories_by_sprint, get_custom_attribute_from_userstory, get_custom_attribute_type_id
 
 
 
@@ -153,3 +156,140 @@ def get_cost_of_delay_for_tasks(sprint_id, auth_token):
     except Exception as e:
         print("Unexpected error calculating cost:")
         return None
+    
+
+def get_cost_of_delay_for_sprint(project_id, sprint_id, auth_token):
+
+    # get issues of a project. -> https://api.taiga.io/api/v1/issues?project=1525740  (completed)
+    
+    # group issues based on milestone (completed)
+    # if milestone == sprint_id then issues.append(issue_id) (completed)
+     
+    #res = {} (completed)
+    #res["0"] = 0 (completed)
+     
+    # loop through dates of sprint (completed)
+        # initialize res[date] = 0 (completed)
+
+    # loop through issues (completed)
+        # stories = https://api.taiga.io/api/v1/issues/custom-attributes-values/{issue_id} (completed)
+        # storyList = stories.split(','). (completed)
+        # dependencies[issue_id] = storyList
+    
+    # loop through dependencies
+        # loop through storyList for each issue_id
+            # take the userstory #id and match the #id with ref in userstory to get the actual userstory_id
+            # get storypoints/business_value (val) with userstory_id
+            # if story_created_date < issue_created_date 
+            # res[issue_created_date] += val
+    
+    # calculate cumulative 
+    # prev_date = 0
+    # loop through res dates
+        # res[date] += res[prev_date]
+        # prev_date = date
+
+    issue_list = get_issues_by_project_id(project_id, auth_token)
+
+    sprint_issue_list = []
+
+    for issue in issue_list :
+        if issue['milestone'] == sprint_id :
+            sprint_issue_list.append(issue)
+
+    response_userstory = {}
+    response_business_value = {}
+
+    sprint_data = get_milestone_by_id(sprint_id, auth_token)
+
+    response_userstory["0"] = 0
+    response_business_value["0"] = 0
+
+    start_date = sprint_data['estimated_start']
+    end_date = sprint_data['estimated_finish']
+
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+
+    for date in range((end_date - start_date).days+1):
+        current_date = start_date+timedelta(days = date)
+        response_userstory[current_date.strftime("%Y-%m-%d")] = 0
+        response_business_value[current_date.strftime("%Y-%m-%d")] = 0
+
+    custom_attribute_type_id = get_custom_attribute_type_id_for_issue(project_id, auth_token, "blocked")
+
+    dependencies = {}
+    issue_data = {}
+
+    for issue in sprint_issue_list :
+        issue_id = issue['id']
+        custom_attribute_value_for_issue = get_custom_attribute_value_by_issue_id(issue_id, custom_attribute_type_id, auth_token)
+        # print(custom_attribute_value_for_issue)
+        issue_data[issue_id] = issue
+        issue_dependency_story_list = [x.strip() for x in custom_attribute_value_for_issue.split(',')] 
+        # print(issue_dependency_story_list)
+        dependencies[issue_id] = issue_dependency_story_list
+
+    userstory_ref_map={}
+
+    user_stories = get_userstories_by_sprint(sprint_id, auth_token)
+    if user_stories and len(user_stories)>0 :
+        for userstory in user_stories :
+            userstory_ref = '#' + str(userstory['ref'])
+            userstory_ref_map[userstory_ref] = userstory
+            # print(userstory_ref, userstory['id'])
+
+    for dependency_key, dependency_value in dependencies.items() :
+        for userstory_label in dependency_value :
+            # print(userstory_label)
+            if userstory_label in userstory_ref_map:
+                userstory = userstory_ref_map[userstory_label]
+                # print(issue_data[dependency_key])
+                if userstory['created_date'] < issue_data[dependency_key]['created_date'] :
+
+                    # print(userstory['total_points'])
+                    userstory_id = userstory['id']
+                    custom_attribute_data = get_custom_attribute_from_userstory(userstory_id, auth_token)
+                    custom_attribute_type_id = get_custom_attribute_type_id(project_id, auth_token, "Business Value")
+                    # print(custom_attribute_type_id)
+
+                    # print(custom_attribute_data)
+                    issue_created_date = issue_data[dependency_key]['created_date']
+                    issue_created_date = datetime.strptime(issue_created_date,"%Y-%m-%dT%H:%M:%S.%fZ")
+                    
+                    print("created date", issue_created_date)
+                    print("estimated start", sprint_data['estimated_start'])
+                    print("estimated finish", sprint_data['estimated_finish'])
+
+                    if sprint_data['estimated_start'] > issue_data[dependency_key]['created_date'] :
+                        response_userstory["0"] += userstory['total_points']
+                        response_business_value["0"] += custom_attribute_data[custom_attribute_type_id]
+                    elif sprint_data['estimated_start'] < issue_data[dependency_key]['created_date'] and sprint_data['estimated_finish'] > issue_data[dependency_key]['created_date']:
+                        response_userstory[issue_created_date.strftime("%Y-%m-%d")] += userstory['total_points']
+                        response_business_value[issue_created_date.strftime("%Y-%m-%d")] += int(custom_attribute_data[custom_attribute_type_id])
+
+                    response_prev = "0"
+                    for response_userstory_key, response_userstory_value in response_userstory.items():
+                        if response_userstory_key != "0":
+                            response_userstory[response_userstory_key] += response_userstory[response_prev]
+                            response_business_value[response_userstory_key] += response_business_value[response_prev]
+                            response_prev = response_userstory_key
+
+                    response = {}
+
+                    response['userstory'] = response_userstory
+                    response['business_value'] = response_business_value
+
+                    # print(response)
+
+                    return response
+
+
+
+
+
+
+
+    
+            
+     
