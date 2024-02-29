ab7c89650b72d8b75cdfc696f33c5bc0dd31487b
avijay48 <avijay48@asu.edu.in>
2024-02-29 15:27:57 -0700

    added unit tests for userstory

--- taigaProject/backend/taigaApi/milestone/getMilestoneById.py
+++ taigaProject/backend/taigaApi/milestone/getMilestoneById.py
@@ -1,6 +1,11 @@
 import requests
 import os
 
+class MilestoneFetchingError(Exception):
+    def __init__(self, status_code, reason):
+        self.status_code = status_code
+        self.reason = reason
+
 def get_milestone_by_id(milestone_id, auth_token):
 
 
@@ -15,14 +20,22 @@ def get_milestone_by_id(milestone_id, auth_token):
     try:
         response = requests.get(taiga_api_url, headers= headers)
         response.raise_for_status()
+        print("response status code---------------",response.status_code)
+        if response.status_code == 401:
+            raise MilestoneFetchingError("UNAUTHORIZED_TOKEN", "401 Client Error: Unauthorized")
 
         milestone_info = response.json()
 
         return milestone_info
     
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error fetching UserStory: {e}")
+        raise MilestoneFetchingError(e.response.status_code, e.response.reason)
+
+    except requests.exceptions.ConnectionError as e:
+        print(f"Connection error fetching UserStory: {e}")
+        raise MilestoneFetchingError("CONNECTION_ERROR", str(e))
+
     except requests.exceptions.RequestException as e:
         print(f'error fetching milestones:{e}')
         return None
-
-
-
