90f615199e2702434840b0d167521c9d9f5223e9
rnolas96 <rnolas96@asu.edu>
2024-03-23 23:22:37 -0700

    Backend cycle-time changes along with basic UI for functionality testing[US #140]

--- taigaProject/backend/controller/task_controller.py
+++ taigaProject/backend/controller/task_controller.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Header, Request, HTTPException
-from service.task_service import get_sprintwise_task_cycle_time, get_sprintwise_task_lead_time, get_cost_of_delay_for_sprint
+from service.task_service import get_sprintwise_task_cycle_time, get_task_cycle_time_time_range, get_sprintwise_task_lead_time, get_task_lead_time_time_range, get_cost_of_delay_for_sprint
 
 task_router = APIRouter()
 
@@ -28,9 +28,18 @@ def get_task_cycle_time(request: Request, project_id: int):
         return get_sprintwise_task_cycle_time(project_id, auth_token)
     else:
         raise HTTPException(status_code = 401, detail = "Missing or invalid access token")
-    
+
+@task_router.get("/cycle_time_time_range")
+def get_task_cycle_time_in_time_range(request: Request, start_date, end_date, project_id: int):
+    auth_token = request.headers.get('Authorization')
+
+    if (auth_token):
+        return get_task_cycle_time_time_range(project_id, start_date, end_date, auth_token)
+    else:
+        raise HTTPException(status_code = 401, detail = "Missing or invalid access token")    
+
 @task_router.get("/lead_time")
-def get_task_cycle_time(request: Request, project_id: int):
+def get_task_lead_time(request: Request, project_id: int):
     auth_token = request.headers.get('Authorization')
 
     if (auth_token):
@@ -38,7 +47,14 @@ def get_task_cycle_time(request: Request, project_id: int):
     else:
         raise HTTPException(status_code = 401, detail = "Missing or invalid access token")
 
+@task_router.get("/lead_time_time_range")
+def get_task_lead_time_in_time_range(request: Request, start_date, end_date, project_id: int):
+    auth_token = request.headers.get('Authorization')
 
+    if (auth_token):
+        return get_task_lead_time_time_range(project_id, start_date, end_date, auth_token)
+    else:
+        raise HTTPException(status_code = 401, detail = "Missing or invalid access token")
     
 @task_router.get("/cost_of_delay")
 def get_cost_of_delay(request: Request, project_id: int, sprint_id: int, business_value_cost_factor: int):
