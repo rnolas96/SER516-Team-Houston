37f41b55c16cf102e31e399d5e723b8c240de728
avijay48 <avijay48@asu.edu.in>
2024-03-24 22:22:10 -0700

    modified docker compose, docker.bat

--- taigaProject/dockerapp.bat
+++ taigaProject/dockerapp.bat
@@ -2,8 +2,8 @@
 
 REM Pull Docker images
 echo Pulling Docker images...
-docker pull akashvj98/taigabackend:dev
-docker pull akashvj98/taigafrontend:dev
+docker pull  avijay48/prod-taigaproject-backend:prod  
+docker pull  avijay48/prod-taigaproject-frontend:prod  
 docker pull redis:latest
 
 REM Check if the pull was successful
@@ -14,4 +14,4 @@ if %errorlevel% neq 0 (
 
 REM Run Docker Compose to create containers
 echo Creating containers...
-docker-compose up -d
+docker-compose -p taigaprojectprod up -d
