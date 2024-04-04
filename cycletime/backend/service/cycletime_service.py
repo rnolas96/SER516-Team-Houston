714467b3b5f1b4c49275fafb6ef87f943a23b2a8
avijay48 <avijay48@asu.edu.in>
2024-04-04 14:06:18 -0700

    modified cycletime,sbpbcoupling service

--- cycletime/backend/service/cycletime_service.py
+++ cycletime/backend/service/cycletime_service.py
@@ -4,7 +4,7 @@ import threading
 import redis
 
 from taigaApi.task.getTaskHistory import get_cycle_time
-from taigaApi.task.getTasks import TaskFetchingError, get_closed_tasks
+from taigaApi.task.getTasks import TaskFetchingError, get_closed_tasks, get_milestone_name
 
 
 r_task = redis.StrictRedis(host='prod-sb-pb-coupling-redis-container', port=6379, db=1)
@@ -91,6 +91,35 @@ def get_task_cycle_time_time_range(project_id, start_date, end_date, auth_token)
         if serialized_cached_data != serialized_response:
             cycle_time_redis_id = str(project_id) + '_' + start_date + '_' + end_date
             r_task.set(f'cycle_time_data_for_date_range:{cycle_time_redis_id}', serialized_response)
+
+        return closed_tasks_response
+
+    return {}
+# Function to calculate  cycle time for tasks which belong to a specific sprint
+def get_sprintwise_task_cycle_time(project_id, auth_token):
+    closed_tasks = get_closed_tasks(project_id, auth_token)
+    milestones_name = get_milestone_name(project_id, auth_token)
+
+    closed_tasks_response = {}
+
+    if closed_tasks is not None:
+        for closed_task in closed_tasks:
+            sprint_id = closed_task.get("milestone_id")
+            sprint_name = milestones_name.get(sprint_id)
+
+            if closed_tasks_response.get(sprint_name) is not None:
+                task_response = closed_tasks_response.get(sprint_name)
+                task_response.append({
+                    "task_id": closed_task.get("id"),
+                    "cycle_time": get_cycle_time(closed_task, auth_token)
+                })
+
+                closed_tasks_response[sprint_name] = task_response 
+            else:
+                closed_tasks_response[sprint_name] = [{
+                    "task_id": closed_task.get("id"),
+                    "cycle_time": get_cycle_time(closed_task, auth_token)
+                }]
 
         return closed_tasks_response
 
