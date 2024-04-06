b2e1d9acbf70a70bc98369f58d7bfbfeecded02a
avijay48 <avijay48@asu.edu.in>
2024-04-06 12:52:16 -0700

    added task coupling

---
+++ taskCoupling/backend/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from controller.taskCouplingController import task_coupling_router
+
+app = FastAPI()
+
+app.include_router(task_coupling_router, prefix='/api/taskCoupling')
