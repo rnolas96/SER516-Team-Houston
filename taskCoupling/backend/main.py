60ea23496bc83ce53012fe49e2e8892e6877fb3f
avijay48 <avijay48@asu.edu.in>
2024-04-07 11:21:58 -0700

    added login router for all microservices

--- taskCoupling/backend/main.py
+++ taskCoupling/backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from controller.taskCouplingController import task_coupling_router
 from controller.project_controller import project_router
+from controller.login_controller import login_router
 
 
 app = FastAPI()
 
 app.include_router(task_coupling_router, prefix='/api/taskCoupling')
 app.include_router(project_router, prefix='/api/project')
+app.include_router(login_router, prefix='/api')
