822613554058efb7ba41bb009d3afbfafd7eae4f
vshar117 <vshar117@asu.edu>
2024-04-07 19:04:16 -0700

    added demo-app

---
+++ demoApp/frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <iframe src="http://localhost:3000" title="Team Houston Burndown" style={{width: '100%', height: '100vh'}} />
+    </div>
+  );
+}
+
+export default App;
