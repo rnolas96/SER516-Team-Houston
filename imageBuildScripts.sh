450e60f9d2e678f4bfdcff877c1f8dda92b4a5bd
avijay48 <157732955+avijay48@users.noreply.github.com>
2024-04-27 17:13:53 -0700

    Update imageBuildScripts.sh

--- imageBuildScripts.sh
+++ imageBuildScripts.sh
@@ -53,18 +53,131 @@ echo "Successfully built all the microservices images"
 read -p "Press Enter to start containers..."
 
 # Start containers
-docker run -d sbpbcouping-metric-backend
-docker run --name sbpbcoupling-api-container -d sbpbcouping-metric-frontend
-docker run -d engagement-metric-backend
-docker run --name api-container-engagement -d engagement-metric-frontend
-docker run -d burndown-metric-backend
-docker run --name api-container-burndown -d burndown-metric-frontend
-docker run -d costofdelay-metric-backend
-docker run --name api-container-costofdelay -d costofdelay-metric-frontend
-docker run -d cycletime-metric-backend
-docker run --name cycletime-api-container -d cycletime-metric-frontend
-docker run -d leadtime-metric-backend
-docker run --name leadime-api-container -d leadtime-metric-frontend
-docker run -d taskcoupling-metric-backend
-docker run --name taskcoupling-api-container -d taskcoupling-metric-frontend
-docker run -d demoapp-frontend
+
+
+
+docker run -d ^
+    --name cycletime-api-container ^
+    -e TAIGA_URL=https://api.taiga.io/api/v1 ^
+    -p 8085:8000 ^
+    cycletime-metric-backend
+
+docker run -d ^
+--name cycletime-redis-container ^
+-p 6385:6379 ^
+-v prod-cycletime-redis_data:/data ^
+redis
+
+docker run -d ^
+    --name cycletime-gui-container ^
+    -p 3035:80 ^
+   cycletime-metric-frontend
+
+
+
+
+docker run -d ^
+    --name api-container-burndown ^
+    -e TAIGA_URL=https://api.taiga.io/api/v1 ^
+    -p 8080:8000 ^
+     burndown-metric-backend
+docker run -d ^
+--name burndown-redis-container ^
+-p 6370:6379 ^
+-v prod-burndown-redis_data:/data ^
+redis
+
+docker run -d ^
+    --name  gui-container-burndown ^
+    -p 3000:80 ^
+    --link api-container-burndown ^
+ burndown-metric-frontend
+
+
+
+
+
+
+ docker run -d ^
+    --name taskcoupling-api-container ^
+    -e TAIGA_URL=https://api.taiga.io/api/v1 ^
+    -p 8084:8000 ^
+    taskcoupling-metric-backend
+     
+
+
+
+docker run -d ^
+    --name taskcoupling-gui-container ^
+    -p 3004:80 ^
+    --link taskcoupling-api-container ^
+taskcoupling-metric-frontend
+
+ docker run -d ^
+    --name leadime-api-container ^
+    -e TAIGA_URL=https://api.taiga.io/api/v1 ^
+    -p 8086:8000 ^
+      leadtime-metric-backend
+
+
+
+docker run -d ^
+    --name leadtime-gui-container ^
+    -p 3036:80 ^
+    --link leadime-api-container ^
+ leadtime-metric-frontend
+
+
+
+ docker run -d ^
+    --name  sbpbcoupling-api-container ^
+    -e TAIGA_URL=https://api.taiga.io/api/v1 ^
+    -p 8082:8000 ^
+	sbpbcouping-metric-backend
+
+
+docker run -d ^
+    --name sbpbcoupling-gui-container ^
+    -p 3002:80 ^
+    --link  sbpbcoupling-api-container ^
+ sbpbcouping-metric-frontend
+
+ 
+ docker run -d ^
+    --name  api-container-costofdelay ^
+    -e TAIGA_URL=https://api.taiga.io/api/v1 ^
+    -p 8081:8000 ^
+	costofdelay-metric-backend
+
+docker run -d ^
+--name redis-container-costofdelay ^
+-p 6369:6379 ^
+-v  prod-redis_data:/data ^
+redis
+
+
+docker run -d ^
+    --name gui-container-costofdelay ^
+    -p 3006:80 ^
+    --link api-container-costofdelay ^
+ costofdelay-metric-frontend
+
+ 
+ docker run -d ^
+    --name  api-container-engagement ^
+    -e TAIGA_URL=https://api.taiga.io/api/v1 ^
+    -p 8005:8000 ^
+	engagement-metric-backend
+
+
+
+docker run -d ^
+    --name  gui-container-engagement ^
+    -p 3005:80 ^
+    --link api-container-engagement ^
+engagement-metric-frontend
+
+docker run -d ^
+    --name  gui-container-demoapp ^
+    -p 3001:80 ^
+   demoapp-frontend
